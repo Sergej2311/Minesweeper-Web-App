815648c83a4892cb3ccf1427237d5a84f0b2a2eb
Sergej <sergej231104@gmail.com>
2024-07-06 13:22:48 +0200

    added Minesweeper board in Frontend

--- frontend/src/app/components/game-board/game-board.component.ts
+++ frontend/src/app/components/game-board/game-board.component.ts
@@ -1,29 +1,31 @@
 import {Component, OnInit} from '@angular/core';
-import {Gameboard} from "./minesweeper";
+import {Gameboard, Tile} from "./minesweeper";
 import {GameBoardService} from "./game-board.service";
-import {DatePipe, NgForOf} from "@angular/common";
+import {NgFor, NgIf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {HttpErrorResponse} from "@angular/common/http";
+import {TileService} from "./tile.service";
 
 @Component({
   selector: 'app-game-board',
   standalone: true,
   imports: [
-    NgForOf,
+    NgFor,
     FormsModule,
-    DatePipe
+    NgIf
   ],
   templateUrl: './game-board.component.html',
   styleUrl: './game-board.component.css'
 })
 export class GameBoardComponent implements OnInit{
-  // @ts-ignore
-  public gameBoards: Gameboard[];
+  public gameBoards: Gameboard[] = [];
+  public tiles: Tile[] = [];
 
-  constructor(private gameboardService: GameBoardService) { }
+  constructor(private gameboardService: GameBoardService, private tileService: TileService) { }
 
   ngOnInit() {
     this.getGameBoards();
+    this.geTiles();
   }
 
   public getGameBoards(): void {
@@ -35,5 +37,20 @@ export class GameBoardComponent implements OnInit{
         alert(error.message);
       }
     );
+  }
+
+  public geTiles(): void {
+    this.tileService.getTiles().subscribe(
+      (response: Tile[]) => {
+        this.tiles = response;
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public startGame(): void {
+    this.gameboardService.startGame();
   }
 }
---
+++ frontend/src/app/components/game-board/tile.service.ts
@@ -0,0 +1,15 @@
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Tile} from "./minesweeper";
+import {Injectable} from "@angular/core";
+
+@Injectable({providedIn: 'root'})
+export class TileService {
+  private apiUrl = 'http://localhost:8080';
+  constructor(private http: HttpClient) { }
+
+  public getTiles(): Observable<Tile[]> {
+    return this.http.get<Tile[]>(`${this.apiUrl}/getAllTiles`)
+  }
+
+}
